ec2b2008516b514bb2ab6a33c3a36e7e5ab4fb41
Mikołaj Piętka <mikolaj.pietka98@gmail.com>
2024-08-22 15:08:13 +0200

    Added date reference into downloaded file

--- index.php
+++ index.php
@@ -981,7 +981,8 @@ if (isset($_POST['single'])) {
 
             csvFile = new Blob([csvData],{type: "tex/csv"});
             let tempLink = document.createElement("a");
-            tempLink.download = "Ingredients.csv";
+            let d = new Date;
+            tempLink.download = "Ingredients-" + d.getFullYear() + ((d.getMonth()+1 < 10) ? "0"+(d.getMonth()+1) : (d.getMonth()+1)) + ((d.getDate() < 10) ? "0"+d.getDate() : d.getDate()) + "-" + ((d.getHours() < 10) ? "0"+d.getHours() : d.getHours()) + ((d.getMinutes() < 10) ? "0"+d.getMinutes() : d.getMinutes()) + ((d.getSeconds() < 10) ? "0"+d.getSeconds() : d.getSeconds()) + ".csv";
             tempLink.href = window.URL.createObjectURL(csvFile);
             tempLink.style.display = "none";
             document.body.appendChild(tempLink);
